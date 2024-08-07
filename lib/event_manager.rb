c7b8ef35c5af5c94fcb3530bee99e7d90923a671
Francisco Acuña <pacman@Franciscos-MacBook-Air.local>
2024-08-06 22:48:01 -0600

    Proyecto Terminado

---
+++ lib/event_manager.rb
@@ -0,0 +1,76 @@
+require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def check_phone(phone)
+  checked_phone = phone.gsub(/\D/, "") # Elimina todos los caracteres no numéricos
+
+  case checked_phone.length
+  when 0..9
+    return "bad number"
+  when 10
+    return checked_phone
+  when 11
+    if checked_phone[0] == '1'
+      checked_phone = checked_phone[1..-1] 
+      return checked_phone
+    else
+      return "bad number"
+    end
+  else
+    return "bad number"
+  end
+end
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  'lib/event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('lib/form_letter.erb')
+erb_template = ERB.new template_letter
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  phone_number = check_phone(row[:homephone])
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id,form_letter)
+  puts form_letter
+end
